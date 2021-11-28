a05b0645490d7bc430d11f125503f870d6abd8f6
Paul Gichuki <pherndegz@gmail.com>
2021-11-28 13:13:56 +0300

    Implement a custom scrollbar for scrollable horizontal container

--- src/components/MoviesContainer.vue
+++ src/components/MoviesContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="movies" :class="{'horizontal': horizontal}">
+  <div class="movies" :class="{ horizontal: horizontal }">
     <MovieCard v-for="movie in movies" :key="movie.id" :movie="movie" />
   </div>
 </template>
@@ -28,6 +28,18 @@ export default {
     justify-content: left;
     overflow-x: auto;
     white-space: nowrap;
+  }
+  .horizontal::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px rgba(82, 82, 82, 0.3);
+  }
+  .horizontal::-webkit-scrollbar {
+    width: 1em;
+  }
+  .horizontal::-webkit-scrollbar-thumb {
+    background-color: #20202b;
+    outline: 1px solid rgb(54, 54, 54);
+    border-radius: 2px;
+    margin: 10px 0;
   }
 }
 </style>
